8197d95269adb99b7458922214d6ccf6d9993a0b
torco34 <torcoromaarias@gimail.com>
2021-01-19 11:28:27 -0500

    section del hederlisto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import '../assets/styles/App.scss';
-
 const Header = () => (
+<header>
+<div className="hero">
+<div className="hero_container">
+ <div className="img">
+	<img src="img/descarga.jpeg" />
+</div>
+		<h1 className="titulo">
+		 Curso de Grid
+		</h1>
 
-<header className="style_header">
-		<nav className="style_lista">
-			<ul>
-				<li>Lugare</li>
-				<li>Noticias</li>
-				<li>Hoteles</li>
-				<li>Tiquetes</li>
-				<li>Iniciar section</li>
-			</ul>
-		</nav>
+		<div className="discription">
+		<p>grid es un curso s de diseño dirigidas a los desarrolladores de aplicaciones web. El CSS grid se puede utilizar para lograr muchos diseños diferentes.</p>
+        </div>
+        <form action="" className="form">
+			<p>Registrate a Platzi</p>
+		<input type="text" placeholder="Nombre"/>
+		<input type="text" placeholder="Correo Electronico"/>
+		<button type="submit">Comienza ahora
+		</button>
+	    </form>
+	    <div className="countdown">
+		<p>Lanzamiento:</p>
+		<p>
+			<span>01d</span>
+			<span>03h</span>
+			<span>09m</span>
+			<span>05s</span>
+		</p>
+	</div>
+
+		</div>
+		</div>
 	</header>
 
 
-
-)
-	
+	)
 
 
 export default Header
