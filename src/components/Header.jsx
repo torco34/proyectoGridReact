9b9555d7dac02ebfb55987a0211d5da3e4ab7e3b
torco34 <torcoromaarias@gimail.com>
2021-01-19 09:14:44 -0500

    slint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import '../assets/styles/App.scss';
 
 const Header = () => (
-<h1>Hola Mundo :</h1>
+
+<header className="style_header">
+		<nav className="style_lista">
+			<ul>
+				<li>Lugare</li>
+				<li>Noticias</li>
+				<li>Hoteles</li>
+				<li>Tiquetes</li>
+				<li>Iniciar section</li>
+			</ul>
+		</nav>
+	</header>
+
+
+
 )
 	
 
